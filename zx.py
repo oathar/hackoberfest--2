807bec41de4d6311215cc4d7b0198e79b63d4518
iathar <141308148+oathar@users.noreply.github.com>
2024-10-05 15:07:19 +0530

    Create zx.py
    zxcccc

---
+++ zx.py
@@ -0,0 +1,18 @@
+if..else chain statement
+letter = "A"
+
+if letter == "B":
+    print("letter is B")
+
+else:
+
+    if letter == "C":
+        print("letter is C")
+
+    else:
+
+        if letter == "A":
+            print("letter is A")
+
+        else:
+            print("letter isn't A, B and C")
